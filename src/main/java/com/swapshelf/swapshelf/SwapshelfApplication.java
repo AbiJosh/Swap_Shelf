e38d4cd9e46e8bd9e19c22dc77b911d06e7e46ed
AbiJosh <abishekjoshua.26csa@licet.ac.in>
2025-02-05 08:41:24 +0000

    First Commit

---
+++ src/main/java/com/swapshelf/swapshelf/SwapshelfApplication.java
@@ -0,0 +1,13 @@
+package com.swapshelf.swapshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapshelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapshelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swapshelf/swapshelf/SwapshelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swapshelf.swapshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapshelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
